4741f07fe2dc8f72af23521393434a0782c64a31
jillxuu <jill@aptoslabs.com>
2023-11-03 10:07:36 -0700

    [indexer grpc] add table info parser to grpc fullnode

--- ecosystem/indexer-grpc/indexer-grpc-fullnode/src/fullnode_data_service.rs
+++ ecosystem/indexer-grpc/indexer-grpc-fullnode/src/fullnode_data_service.rs
@@ -8,14 +8,16 @@ use aptos_protos::internal::fullnode::v1::{
     fullnode_data_server::FullnodeData, stream_status::StatusType, transactions_from_node_response,
     GetTransactionsFromNodeRequest, StreamStatus, TransactionsFromNodeResponse,
 };
+use deadpool_postgres::Pool;
 use futures::Stream;
-use std::pin::Pin;
+use std::{pin::Pin, sync::Arc};
 use tokio::sync::mpsc;
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::{Request, Response, Status};
 
 pub struct FullnodeDataService {
     pub service_context: ServiceContext,
+    pub pool: Arc<Pool>,
     pub enable_verbose_logging: bool,
 }
 
@@ -61,7 +63,7 @@ impl FullnodeData for FullnodeDataService {
 
         // Creates a moving average to track tps
         let mut ma = MovingAverage::new(10_000);
-
+        let pool = self.pool.clone();
         // This is the main thread handling pushing to the stream
         tokio::spawn(async move {
             // Initialize the coordinator that tracks starting version and processes transactions
