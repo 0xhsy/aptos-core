69f74431a507743041db2113ea7fea8774165324
Stelian Ionescu <stelian@aptoslabs.com>
2023-12-11 18:43:41 -0500

    Use text format for Forge CI runs

--- testsuite/run_forge.sh
+++ testsuite/run_forge.sh
@@ -5,6 +5,8 @@
 
 # A light wrapper for the new forge python script
 
+export RUST_LOG_FORMAT=text
+
 echo "Warning: run_forge.sh is deprecated. Please use forge.py instead."
 echo "Executing python testsuite/forge.py test $@"
 exec python3 testsuite/forge.py test "$@"
